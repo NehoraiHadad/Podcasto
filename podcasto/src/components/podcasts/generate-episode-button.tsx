9b88eebaa301791e7b6575c988f94c4fd9087d05
Nehorai Hadad <106696390+NehoraiHadad@users.noreply.github.com>
2025-10-30 23:13:12 +0200

    Re-export admin generate episode button

--- podcasto/src/components/podcasts/generate-episode-button.tsx
+++ podcasto/src/components/podcasts/generate-episode-button.tsx
@@ -1,4 +1,3 @@
-import { EpisodeDateRangePicker } from './episode-date-range-picker';
-import { Info } from 'lucide-react';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
-// TODO: Support scheduling/delayed episode publishing so creators can decide when to make episodes public.
+'use client';
+
+export * from '../admin/generate-episode-button';
